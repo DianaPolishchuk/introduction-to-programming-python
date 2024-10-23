922ba91d123bb007a6e1eab0aa43154c8a223b9e
Diana Polyschuk <dianapolyschuk@gmail.com>
2024-10-23 11:15:14 +0300

    full program

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+print("I'm learning Git merge conflicts")
 name = input('Please enter your name:')
 last_name = input('Please enter your last name:')
-print('Hi, {} {}!'.format(name, last_name))
 nick = input('Please enter your nickname')
-print('Hi, {}!'.format(nick))
+# print('Hi, {}!'.format(nick))
+print('Hi, {} {} a.k.a. {}!'.format(name, last_name, nick))
